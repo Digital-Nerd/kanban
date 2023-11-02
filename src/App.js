2c0d51578c6b79289afa0d2a55faf0e57da4da1a
Digital-Nerd <0809yadavlokesh@gmail.com>
2023-11-02 21:17:10 +0530

    created a kanban dashboard, fetching data from an api. This dashboard is fully hardcoded no css framework used. 2 functionalities remaining: sort by title and keeping the checked card same even after refeshing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from "react";
+import Display from './components/Display';
+import DisplayStatus from './components/DisplayStatus';
+import DisplayUser from './components/DisplayUser';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [check, setCheck] = useState(false);
+
+  const toggleCheck = () => {
+    setCheck(!check);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Display check={check} toggleCheck={toggleCheck} />} />
+        {/* <Route path="/" element={<Display />}/> */}
+        <Route path="/user" element={<DisplayUser check={check} toggleCheck={toggleCheck}/>}/>
+        <Route path="/status" element={<DisplayStatus check={check} toggleCheck={toggleCheck}/>}/>
+      </Routes>
+    </Router>
+
+
+
+      {/* <Cards title="Card 1" tag="Feature Request" id="Cam 11(id)" /> */}
+          
+    </>
   );
 }
 
